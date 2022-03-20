af29eb46e2baa73bacfa9133def3228dd0405622
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-03-21 00:15:20 +0530

    Completed constraints_setup

--- parse_input.py
+++ parse_input.py
@@ -1,5 +1,5 @@
 # File to parse the input file and return the data as python objects
-  
+
 import json
 import os
 from astropy.coordinates import EarthLocation
@@ -7,71 +7,68 @@ from astroplan import Observer
 import astropy.units as units
 
 # Opening JSON file
+# file_path=open("./inputs/observatory.json")
+
 
 def observatory_setup(file_path):
     """
     Parse input observatory.json file
-    
+
     Inputs:
         Absolute or relative path of the input json file containing
         information about the observatory
     Returns:
         an object of the Observer class from Astroplan
     """
-    
+
     # Getting the absolute path to avoid confusion
     file_path = os.path.abspath(file_path)
     observator_data = open(file_path)
-    
-    
+
     # Parsing the file
     try:
         data = json.load(observator_data)
     except FileNotFoundError as err:
         print("OBSERVATORY JSON FILE NOT FOUND")
         print("The File you provided doesnt exist. Please Check and Enter again")
-    
+
     # Calculating and Returning the object.
-    
-    location = EarthLocation(data['longitude'], 
-                             data['latitude'], 
-                             data['elevation'] * units.m)
-    
+
+    location = EarthLocation(
+        data["longitude"], data["latitude"], data["elevation"] * units.m
+    )
+
     ioMIT = Observer(
         location=location,
-        timezone=data['timezone'],
-        name=data['name'],
-        description=data['description'],
+        timezone=data["timezone"],
+        name=data["name"],
+        description=data["description"],
     )
     print(ioMIT)
     return ioMIT
 
 
-def date_and_time_setup():
+def date_and_time_setup(file_path):
     # return a list of the important dates and times like day and time of observation
     # or return a single astropy date object without formatitng.
     # or return a datetime_object
+
     pass
+
 
 def targets_setup():
     # return a list of Objects of the target class pre initialized.
     pass
 
+
 def constraints_setup():
-    # return a dictionary with constraints in it
-    
-    # read the variables from ./input/constraints.json
-    # something like this
+    file_path = os.path.abspath(file_path)
+    constraints_data = open(file_path)
 
-    #     constraints = {
-    #     "airmass": airmass,
-    #     "minimum_altitude": min_alt,
-    #     "maximum_altitude": max_alt,
-    #     "moon_separation": moon_separation,
-    #     "start_time": start_time,
-    #     "end_time": end_time
-    # }
-    
-    
-    pass
+    try:
+        data = json.load(constraints_data)
+    except FileNotFoundError as err:
+        print("CONSTRAINTS JSON FILE NOT FOUND")
+        print("The File you provided doesnt exist. Please Check and Enter again")
 
+    return data
